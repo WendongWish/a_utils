9a17fce84759a7262127edc91a90395a29ac5f89
WenDong <why_6267@163.com>
2022-04-25 20:33:55 +0800

    Initial Commit.

--- seg_2D/outline2image.py
+++ seg_2D/outline2image.py
@@ -1,39 +1,69 @@
-#!/usr/bin/python3.8
-# -*- coding: utf-8 -*-
-# @Time    : 21-09-09 0009 15:54
-# @Author  : Hy Wang
-# @Software: PyCharm
-
-
+# Tool: PyCharm
+# coding: utf-8
+"""=========================================
+# Project_Name: daily
+# Author: WenDong
+# Date: 2022/4/25 15:25
+# Function:
+# Description:
+========================================="""
 import cv2
-from os.path import join
+from PIL import Image
 
 
-def mask_to_image(save_root, img, mask, pred, mask_line_thickness, pred_line_thickness):
-    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)  # mask
-    pred = cv2.cvtColor(pred, cv2.COLOR_BGR2GRAY)  # pred
+def mask_to_image(save_root, img, mask=None, predict=None, mask_line_thickness=1, prediction_line_thickness=1):
+    """
+    :param save_root: 输出图像的保存路径
+    :param img: 原图的输入路径
+    :param mask: 原图所对应的标签路径
+    :param predict: 预测结果的路径
+    :param mask_line_thickness: 标签的轮廓在原图上所画线的线宽，默认为1
+    :param prediction_line_thickness: 预测结果的轮廓在原图上所画线的线宽，默认为1
+    :return: None(默认保存到save_root)
+    """
+    if img is None:
+        raise Exception("img is None!")
+    else:
+        img = cv2.imread(img, cv2.IMREAD_COLOR)
 
-    # 获取mask的轮廓
-    ret_mask, thresh_mask = cv2.threshold(mask, 127, 255, 0)
-    contours_mask, im_mask = cv2.findContours(thresh_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(image=img, contours=contours_mask, contourIdx=-1, color=(255, 0, 0), thickness=mask_line_thickness)
+    if save_root is None:
+        raise Exception("save_root is None!")
 
-    # 获取predic的轮廓
-    ret_pred, thresh_pred = cv2.threshold(pred, 127, 255, 0)
-    contours_pred, im_pred = cv2.findContours(thresh_pred, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(image=img, contours=contours_pred, contourIdx=-1, color=(0, 255, 0), thickness=pred_line_thickness)
+    if (mask is None) and (predict is None):
+        raise Exception("mask and predict cannot be None at the same time!")
+
+    elif (mask is not None) and (predict is None):
+        ret_mask, thresh_mask = cv2.threshold(cv2.cvtColor(cv2.imread(mask, cv2.IMREAD_COLOR), cv2.COLOR_BGR2GRAY), 127, 255, 0)
+        contours_mask, im_mask = cv2.findContours(thresh_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(image=img, contours=contours_mask, contourIdx=-1, color=(255, 0, 0),
+                         thickness=mask_line_thickness)
+    elif (predict is not None) and (mask is None):
+        ret_prediction, thresh_prediction = cv2.threshold(cv2.cvtColor(cv2.imread(predict, cv2.IMREAD_COLOR), cv2.COLOR_BGR2GRAY), 127, 255, 0)
+        contours_prediction, im_prediction = cv2.findContours(thresh_prediction, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(image=img, contours=contours_prediction, contourIdx=-1, color=(0, 255, 0),
+                         thickness=prediction_line_thickness)
+    else:
+        ret_prediction, thresh_prediction = cv2.threshold(cv2.cvtColor(cv2.imread(predict, cv2.IMREAD_COLOR), cv2.COLOR_BGR2GRAY), 127, 255, 0)
+        contours_prediction, im_prediction = cv2.findContours(thresh_prediction, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(image=img, contours=contours_prediction, contourIdx=-1, color=(0, 255, 0),
+                         thickness=prediction_line_thickness)
+
+        ret_mask, thresh_mask = cv2.threshold(cv2.cvtColor(cv2.imread(mask, cv2.IMREAD_COLOR), cv2.COLOR_BGR2GRAY), 127, 255, 0)
+        contours_mask, im_mask = cv2.findContours(thresh_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(image=img, contours=contours_mask, contourIdx=-1, color=(255, 0, 0),
+                         thickness=mask_line_thickness)
 
     cv2.imwrite(save_root, img)
-    # cv2.namedWindow('a')
-    # cv2.imshow('a', img)
-    # cv2.waitKey(0)
 
 
 if __name__ == '__main__':
-    root = 'G:\Datasets\ISIC2018\ISIC2018_Task1_Test_Input_GT\Iresult\Ia_3smallaim\\None_csSE_1361218_56\\'
-    pic_num = str(19)
-    save_root = join(root, 'result.png')
-    img = cv2.imread(join(root, pic_num + '_image.png'))  #
-    mask = cv2.imread(join(root, pic_num + '_GT.png'))  #
-    pred = cv2.imread(join(root, pic_num + '_SR.png'))  #
-    mask_to_image(save_root, img, mask, pred, 2, 2)
+    img = '../seg/img.jpg'
+    mask = '../seg/mask.png'
+    predict = '../seg/predict.png'
+
+
+    save_root = '../seg/result.png'
+    mask_to_image(save_root=save_root, img=img, mask=mask, predict=None)
+
+    image = Image.open(save_root)
+    image.show()
